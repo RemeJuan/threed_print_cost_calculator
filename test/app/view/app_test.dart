d571898604c09d33ee240abbfc73eaca46b0c16f
Reme Le Hane <mail@remej.dev>
2021-12-26 08:56:51 +0200

    test(app): fixed test mocking

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -6,35 +6,23 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sembast/sembast.dart';
 import 'package:threed_print_cost_calculator/app/app.dart';
 import 'package:threed_print_cost_calculator/calculator/view/calculator_page.dart';
-import 'package:threed_print_cost_calculator/locator.dart';
 
 import '../../helpers/helpers.dart';
-import '../../helpers/mocks.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  late MockDatabase mockDatabase;
-
-  setUp(() {
-    setupTest();
-
-    mockDatabase = MockDatabase();
-    sl.registerSingletonAsync<Database>(() async => mockDatabase);
+  setUp(() async {
+    await setupTest();
   });
 
-  group(
-    'App',
-    () {
-      testWidgets('renders CounterPage', (tester) async {
-        await tester.pumpApp(const App());
-        await tester.pumpAndSettle();
-        expect(find.byType(CalculatorPage), findsOneWidget);
-      });
-    },
-    skip: true,
-  );
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpApp(const App());
+      await tester.pumpAndSettle();
+      expect(find.byType(CalculatorPage), findsOneWidget);
+    });
+  });
 }
