0049cf43f1f7785fc8c9e0a1da77934d9241f5d0
Reme Le Hane <mail@remej.dev>
2021-12-26 08:57:08 +0200

    test(app): [WIP] setting up results test

---
+++ test/calculator/view/calculator_results.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:threed_print_cost_calculator/calculator/view/calculator_results.dart';
+
+import '../../helpers/helpers.dart';
+
+void main() {
+  setUp(() async {
+    // await setupTest();
+  });
+
+  group('CalculatorPage', () {
+    testWidgets('renders CalculatorView', (tester) async {
+      await tester.pumpApp(
+        const CalculatorResults(
+          results: <String, String>{
+            'electricity': '0.00',
+            'filament': '0.00',
+            'total': '0.00',
+          },
+        ),
+      );
+      await tester.pumpAndSettle();
+      expect(find.byType(CalculatorResults), findsOneWidget);
+    });
+  }, skip: true);
+}
