d690135ec4c524eae198a69d5f1607b53bf1d997
Reme Le Hane <mail@remej.dev>
2021-12-27 15:32:39 +0200

    test(app): updated test file

--- test/calculator/view/calculator_results.dart
+++ test/calculator/view/calculator_results.dart
@@ -8,19 +8,23 @@ void main() {
     // await setupTest();
   });
 
-  group('CalculatorPage', () {
-    testWidgets('renders CalculatorView', (tester) async {
-      await tester.pumpApp(
-        const CalculatorResults(
-          results: <String, String>{
-            'electricity': '0.00',
-            'filament': '0.00',
-            'total': '0.00',
-          },
-        ),
-      );
-      await tester.pumpAndSettle();
-      expect(find.byType(CalculatorResults), findsOneWidget);
-    });
-  }, skip: true);
+  group(
+    'CalculatorPage',
+    () {
+      testWidgets('renders CalculatorView', (tester) async {
+        await tester.pumpApp(
+          const CalculatorResults(
+            results: <String, String>{
+              'electricity': '0.00',
+              'filament': '0.00',
+              'total': '0.00',
+            },
+          ),
+        );
+        await tester.pumpAndSettle();
+        expect(find.byType(CalculatorResults), findsOneWidget);
+      });
+    },
+    skip: true,
+  );
 }
