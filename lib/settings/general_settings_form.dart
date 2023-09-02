16bc6b73fbafd9421a61d64c62432c2f0ae3e632
Reme Le Hane <mail@remej.dev>
2023-09-02 14:56:56 +0200

    fix(settings): data structure updates

--- lib/settings/general_settings_form.dart
+++ lib/settings/general_settings_form.dart
@@ -5,6 +5,7 @@ import 'package:sembast/sembast.dart';
 import 'package:threed_print_cost_calculator/database/database_helpers.dart';
 import 'package:threed_print_cost_calculator/l10n/l10n.dart';
 import 'package:threed_print_cost_calculator/locator.dart';
+import 'package:threed_print_cost_calculator/settings/model/general_settings_model.dart';
 
 class GeneralSettings extends HookWidget {
   const GeneralSettings({super.key});
@@ -13,67 +14,53 @@ class GeneralSettings extends HookWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
-    final costController = useTextEditingController();
-    final wattController = useTextEditingController();
-
     final db = sl<Database>();
-    final store = stringMapStoreFactory.store(describeEnum(DBName.settings));
-    final dbHelpers = DataBaseHelpers(DBName.settings);
-
-    final query = store.query();
-
-    store.query().onSnapshots(db).listen((event) {
-      event.forEach((element) {
-        if (element.key == 'electricityCost') {
-          costController.text = element.value['value'] as String;
-        }
-        if (element.key == 'wattage') {
-          wattController.text = element.value['value'] as String;
-        }
-      });
-    });
+    final store = StoreRef.main();
+    final dbHelper = DataBaseHelpers(DBName.settings);
 
     return StreamBuilder(
-      stream: query.onSnapshots(db),
+      stream: store.record(describeEnum(DBName.settings)).onSnapshot(db),
       builder: (context, snapshot) {
+        var data = GeneralSettingsModel.initial();
+
         if (snapshot.hasData) {
-          return Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  controller: costController,
-                  onChanged: (value) async {
-                    await dbHelpers.addOrUpdateRecord(
-                      'electricityCost',
-                      value,
-                    );
-                  },
-                  decoration: InputDecoration(
-                    labelText: l10n.electricityCostLabel,
-                    suffixText: l10n.kwh,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: TextFormField(
-                  controller: wattController,
-                  onChanged: (value) async {
-                    await dbHelpers.addOrUpdateRecord(
-                      'wattage',
-                      value,
-                    );
-                  },
-                  decoration: InputDecoration(
-                    labelText: l10n.wattLabel,
-                    suffixText: l10n.watt,
-                  ),
-                ),
-              ),
-            ],
+          debugPrint(snapshot.data!.value.toString());
+          data = GeneralSettingsModel.fromMap(
+            snapshot.data!.value as Map<String, dynamic>,
           );
         }
-        return const Center(child: CircularProgressIndicator());
+
+        return Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                initialValue: data.electricityCost,
+                onChanged: (value) async {
+                  final updated = data.copyWith(electricityCost: value);
+                  await dbHelper.updateDb(updated.toMap(), DBName.settings);
+                },
+                decoration: InputDecoration(
+                  labelText: l10n.electricityCostLabel,
+                  suffixText: l10n.kwh,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: TextFormField(
+                initialValue: data.wattage,
+                onChanged: (value) async {
+                  final updated = data.copyWith(wattage: value);
+                  await dbHelper.updateDb(updated.toMap(), DBName.settings);
+                },
+                decoration: InputDecoration(
+                  labelText: l10n.wattLabel,
+                  suffixText: l10n.watt,
+                ),
+              ),
+            ),
+          ],
+        );
       },
     );
   }
