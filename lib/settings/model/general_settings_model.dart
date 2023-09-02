35da783c5cd89baebc86ceaa0f20ddfbb6b40519
Reme Le Hane <mail@remej.dev>
2023-09-02 14:58:18 +0200

    feat(settings): added active printer to db model

--- lib/settings/model/general_settings_model.dart
+++ lib/settings/model/general_settings_model.dart
@@ -1,43 +1,51 @@
-
-
 class GeneralSettingsModel {
   const GeneralSettingsModel({
     required this.electricityCost,
     required this.wattage,
+    required this.activePrinter,
   });
 
   factory GeneralSettingsModel.fromMap(Map<String, dynamic> map) {
     return GeneralSettingsModel(
       electricityCost: (map['electricityCost'] ?? '0.0').toString(),
       wattage: (map['wattage'] ?? '0').toString(),
+      activePrinter: (map['activePrinter'] ?? '').toString(),
     );
   }
 
   factory GeneralSettingsModel.initial() {
-    return const GeneralSettingsModel(electricityCost: '0', wattage: '0');
+    return const GeneralSettingsModel(
+      electricityCost: '0',
+      wattage: '0',
+      activePrinter: '',
+    );
   }
 
   final String electricityCost;
   final String wattage;
+  final String activePrinter;
 
   Map<String, dynamic> toMap() {
     return {
       'electricityCost': electricityCost,
       'wattage': wattage,
+      'activePrinter': activePrinter,
     };
   }
 
   GeneralSettingsModel copyWith({
     String? electricityCost,
     String? wattage,
+    String? activePrinter,
   }) {
     return GeneralSettingsModel(
       electricityCost: electricityCost ?? this.electricityCost,
       wattage: wattage ?? this.wattage,
+      activePrinter: activePrinter ?? this.activePrinter,
     );
   }
 
   @override
   String toString() =>
-      'GeneralSettingsModel(electricityCost: $electricityCost, wattage: $wattage)';
+      'GeneralSettingsModel(electricityCost: $electricityCost, wattage: $wattage, activePrinter: $activePrinter)';
 }
