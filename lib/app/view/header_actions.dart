bc5da39632d86fda143caed46dafab8697aaec79
Reme Le Hane <mail@remej.dev>
2023-09-03 06:53:29 +0200

    fix(premium): history has been moved to the tabs

--- lib/app/view/header_actions.dart
+++ lib/app/view/header_actions.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:threed_print_cost_calculator/calculator/view/subscriptions.dart';
-import 'package:threed_print_cost_calculator/history/history_page.dart';
 
 class HeaderActions extends HookWidget {
   const HeaderActions({super.key});
@@ -23,21 +22,16 @@ class HeaderActions extends HookWidget {
     return Container(
       margin: const EdgeInsets.only(right: 16),
       child: premium.value
-          ? IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const HistoryPage()),
-                );
-              },
-              icon: const Icon(Icons.list),
-            )
+          ? const SizedBox.shrink()
           : IconButton(
               onPressed: () async => showModalBottomSheet(
                 context: context,
                 builder: (_) => const Subscriptions(),
               ),
-              icon: const Icon(Icons.attach_money_sharp),
+              icon: const Icon(
+                Icons.attach_money_sharp,
+                color: Colors.white54,
+              ),
             ),
     );
   }
