b45a983801bfe8cae70109f312592428b9219f1b
Reme Le Hane <mail@remej.dev>
2023-09-09 14:12:14 +0200

    fat(settings): wrapped printers within premium check

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:threed_print_cost_calculator/settings/general_settings_form.dart';
 import 'package:threed_print_cost_calculator/settings/printers/printers.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends HookWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final premium = useState<bool>(false);
+    useEffect(
+      () {
+        Purchases.addCustomerInfoUpdateListener((info) {
+          premium.value = info.entitlements.active.isNotEmpty;
+        });
+        return null;
+      },
+      [],
+    );
+
     return ListView(
       padding: const EdgeInsets.all(16),
-      children: const [
-        GeneralSettings(),
-        SizedBox(height: 16),
-        Printers(),
+      children: [
+        const GeneralSettings(),
+        const SizedBox(height: 16),
+        if (premium.value) const Printers(),
         // Materials(),
       ],
     );
