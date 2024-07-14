386f580442331765e284aa53c044c2f5886040c7
Reme Le Hane <reme@loop.co.za>
2024-07-14 17:21:47 +0200

    fix(locale): fixed build issues from localization

---
+++ lib/generated/intl/messages_de.dart
@@ -0,0 +1,66 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a de locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'de';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "calculatorAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("3D-Druck-Rechner"),
+        "electricityCostLabel":
+            MessageLookupByLibrary.simpleMessage("Stromkosten"),
+        "failureRiskLabel":
+            MessageLookupByLibrary.simpleMessage("Ausfallrisiko (%)"),
+        "historyAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("Geschichte"),
+        "hoursLabel":
+            MessageLookupByLibrary.simpleMessage("Druckzeit (Stunden)"),
+        "kwh": MessageLookupByLibrary.simpleMessage("kW/h"),
+        "labourCostPrefix":
+            MessageLookupByLibrary.simpleMessage("Arbeitskosten: "),
+        "labourRateLabel": MessageLookupByLibrary.simpleMessage("Stundensatz"),
+        "labourTimeLabel":
+            MessageLookupByLibrary.simpleMessage("Bearbeitungszeit"),
+        "minutesLabel": MessageLookupByLibrary.simpleMessage("Protokoll"),
+        "premiumHeader":
+            MessageLookupByLibrary.simpleMessage("Nur Premium-Benutzer:"),
+        "printWeightLabel":
+            MessageLookupByLibrary.simpleMessage("Gewicht des Drucks"),
+        "resultElectricityPrefix":
+            MessageLookupByLibrary.simpleMessage("Gesamtkosten für Strom:"),
+        "resultFilamentPrefix":
+            MessageLookupByLibrary.simpleMessage("Gesamtkosten für Filament:"),
+        "resultTotalPrefix":
+            MessageLookupByLibrary.simpleMessage("Gesamtkosten: "),
+        "riskTotalPrefix":
+            MessageLookupByLibrary.simpleMessage("Risikokosten:"),
+        "settingsAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("Einstellungen"),
+        "spoolCostLabel":
+            MessageLookupByLibrary.simpleMessage("Spulen-/Harzkosten"),
+        "spoolWeightLabel":
+            MessageLookupByLibrary.simpleMessage("Spule/Harzgewicht"),
+        "submitButton": MessageLookupByLibrary.simpleMessage("Berechnung"),
+        "watt": MessageLookupByLibrary.simpleMessage("Watt"),
+        "wattLabel": MessageLookupByLibrary.simpleMessage("Watt (3D-Drucker)"),
+        "wearAndTearLabel":
+            MessageLookupByLibrary.simpleMessage("Materialien/Verschleiß")
+      };
+}
