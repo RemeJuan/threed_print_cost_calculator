50e4e05924dfdfd36cea171491bca4206ead8bf2
Reme Le Hane <mail@remej.dev>
2024-07-27 09:39:54 +0200

    refactor(locale): making form inputs nullable for better UI

--- lib/app/components/int_input.dart
+++ lib/app/components/int_input.dart
@@ -4,16 +4,16 @@ import 'package:formz/formz.dart';
 enum IntInputError { invalid }
 
 // Extend FormzInput and provide the input type and error type.
-class IntInput extends FormzInput<int, IntInputError> {
+class IntInput extends FormzInput<int?, IntInputError> {
   // Call super.pure to represent an unmodified form input.
-  const IntInput.pure() : super.pure(-1);
+  const IntInput.pure() : super.pure(null);
 
   // Call super.dirty to represent a modified form input.
-  const IntInput.dirty({int value = 0}) : super.dirty(value);
+  const IntInput.dirty({int? value = 0}) : super.dirty(value);
 
   // Override validator to handle validating a given input value.
   @override
-  IntInputError? validator(int value) {
-    return value > -1 ? IntInputError.invalid : null;
+  IntInputError? validator(int? value) {
+    return (value != null && value > -1) ? IntInputError.invalid : null;
   }
 }
