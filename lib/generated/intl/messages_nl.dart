fdcdc51fc5aa8d7a1ef4316ef8f59ec3dafa95b4
Reme Le Hane <reme@loop.co.za>
2024-12-07 14:00:47 +0200

    feat(locale): added dutch translations

---
+++ lib/generated/intl/messages_nl.dart
@@ -0,0 +1,65 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a nl locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'nl';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "calculatorAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("3D-printcalculator"),
+        "electricityCostLabel":
+            MessageLookupByLibrary.simpleMessage("Elektriciteitskosten"),
+        "failureRiskLabel":
+            MessageLookupByLibrary.simpleMessage("Risico op falen (%)"),
+        "historyAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("Geschiedenis"),
+        "hoursLabel": MessageLookupByLibrary.simpleMessage("Afdruktijd (uren)"),
+        "kwh": MessageLookupByLibrary.simpleMessage("kW/uur"),
+        "labourCostPrefix":
+            MessageLookupByLibrary.simpleMessage("Arbeidskosten:"),
+        "labourRateLabel": MessageLookupByLibrary.simpleMessage("Uurtarief"),
+        "labourTimeLabel":
+            MessageLookupByLibrary.simpleMessage("Verwerkingstijd"),
+        "minutesLabel": MessageLookupByLibrary.simpleMessage("Notulen"),
+        "premiumHeader": MessageLookupByLibrary.simpleMessage(
+            "Alleen voor Premium-gebruikers:"),
+        "printWeightLabel":
+            MessageLookupByLibrary.simpleMessage("Gewicht van de afdruk"),
+        "resultElectricityPrefix": MessageLookupByLibrary.simpleMessage(
+            "Totale kosten voor elektriciteit:"),
+        "resultFilamentPrefix": MessageLookupByLibrary.simpleMessage(
+            "Totale kosten voor filament:"),
+        "resultTotalPrefix":
+            MessageLookupByLibrary.simpleMessage("Totale kosten:"),
+        "riskTotalPrefix":
+            MessageLookupByLibrary.simpleMessage("Risicokosten:"),
+        "settingsAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("Instellingen"),
+        "spoolCostLabel":
+            MessageLookupByLibrary.simpleMessage("Spoel/hars kosten"),
+        "spoolWeightLabel":
+            MessageLookupByLibrary.simpleMessage("Spoel/harsgewicht"),
+        "submitButton": MessageLookupByLibrary.simpleMessage("Berekenen"),
+        "watt": MessageLookupByLibrary.simpleMessage("Watt"),
+        "wattLabel": MessageLookupByLibrary.simpleMessage("Watt (3D-printer)"),
+        "wearAndTearLabel":
+            MessageLookupByLibrary.simpleMessage("Materialen/Slijtage")
+      };
+}
