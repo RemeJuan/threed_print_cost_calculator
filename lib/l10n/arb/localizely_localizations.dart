2d5b51578d369691352d4acf15e2119d3e52cbf3
Reme Le Hane <reme@loop.co.za>
2026-04-01 12:21:50 +0200

    chore(l10n): Format generated localization delegate

--- lib/l10n/arb/localizely_localizations.dart
+++ lib/l10n/arb/localizely_localizations.dart
@@ -9,136 +9,214 @@ import 'app_localizations.dart';
 class LocalizelyLocalizations extends AppLocalizations {
   final AppLocalizations _fallback;
 
-  LocalizelyLocalizations(String locale, AppLocalizations fallback) : _fallback = fallback, super(locale);
+  LocalizelyLocalizations(String locale, AppLocalizations fallback)
+    : _fallback = fallback,
+      super(locale);
 
-  static const LocalizationsDelegate<AppLocalizations> delegate = _LocalizelyLocalizationsDelegate();
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _LocalizelyLocalizationsDelegate();
 
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
-    delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ];
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
 
-  static const List<Locale> supportedLocales = AppLocalizations.supportedLocales;
-
-  @override
-  String get calculatorAppBarTitle => LocalizelyGenL10n.getText(localeName, 'calculatorAppBarTitle') ?? _fallback.calculatorAppBarTitle;
-
-  @override
-  String get historyAppBarTitle => LocalizelyGenL10n.getText(localeName, 'historyAppBarTitle') ?? _fallback.historyAppBarTitle;
-
-  @override
-  String get settingsAppBarTitle => LocalizelyGenL10n.getText(localeName, 'settingsAppBarTitle') ?? _fallback.settingsAppBarTitle;
+  static const List<Locale> supportedLocales =
+      AppLocalizations.supportedLocales;
 
   @override
-  String get wattLabel => LocalizelyGenL10n.getText(localeName, 'wattLabel') ?? _fallback.wattLabel;
+  String get calculatorAppBarTitle =>
+      LocalizelyGenL10n.getText(localeName, 'calculatorAppBarTitle') ??
+      _fallback.calculatorAppBarTitle;
 
   @override
-  String get printWeightLabel => LocalizelyGenL10n.getText(localeName, 'printWeightLabel') ?? _fallback.printWeightLabel;
+  String get historyAppBarTitle =>
+      LocalizelyGenL10n.getText(localeName, 'historyAppBarTitle') ??
+      _fallback.historyAppBarTitle;
 
   @override
-  String get hoursLabel => LocalizelyGenL10n.getText(localeName, 'hoursLabel') ?? _fallback.hoursLabel;
+  String get settingsAppBarTitle =>
+      LocalizelyGenL10n.getText(localeName, 'settingsAppBarTitle') ??
+      _fallback.settingsAppBarTitle;
 
   @override
-  String get wearAndTearLabel => LocalizelyGenL10n.getText(localeName, 'wearAndTearLabel') ?? _fallback.wearAndTearLabel;
+  String get wattLabel =>
+      LocalizelyGenL10n.getText(localeName, 'wattLabel') ?? _fallback.wattLabel;
 
   @override
-  String get labourRateLabel => LocalizelyGenL10n.getText(localeName, 'labourRateLabel') ?? _fallback.labourRateLabel;
+  String get printWeightLabel =>
+      LocalizelyGenL10n.getText(localeName, 'printWeightLabel') ??
+      _fallback.printWeightLabel;
 
   @override
-  String get labourTimeLabel => LocalizelyGenL10n.getText(localeName, 'labourTimeLabel') ?? _fallback.labourTimeLabel;
+  String get hoursLabel =>
+      LocalizelyGenL10n.getText(localeName, 'hoursLabel') ??
+      _fallback.hoursLabel;
 
   @override
-  String get failureRiskLabel => LocalizelyGenL10n.getText(localeName, 'failureRiskLabel') ?? _fallback.failureRiskLabel;
+  String get wearAndTearLabel =>
+      LocalizelyGenL10n.getText(localeName, 'wearAndTearLabel') ??
+      _fallback.wearAndTearLabel;
 
   @override
-  String get minutesLabel => LocalizelyGenL10n.getText(localeName, 'minutesLabel') ?? _fallback.minutesLabel;
+  String get labourRateLabel =>
+      LocalizelyGenL10n.getText(localeName, 'labourRateLabel') ??
+      _fallback.labourRateLabel;
 
   @override
-  String get spoolWeightLabel => LocalizelyGenL10n.getText(localeName, 'spoolWeightLabel') ?? _fallback.spoolWeightLabel;
+  String get labourTimeLabel =>
+      LocalizelyGenL10n.getText(localeName, 'labourTimeLabel') ??
+      _fallback.labourTimeLabel;
 
   @override
-  String get spoolCostLabel => LocalizelyGenL10n.getText(localeName, 'spoolCostLabel') ?? _fallback.spoolCostLabel;
+  String get failureRiskLabel =>
+      LocalizelyGenL10n.getText(localeName, 'failureRiskLabel') ??
+      _fallback.failureRiskLabel;
 
   @override
-  String get electricityCostLabel => LocalizelyGenL10n.getText(localeName, 'electricityCostLabel') ?? _fallback.electricityCostLabel;
+  String get minutesLabel =>
+      LocalizelyGenL10n.getText(localeName, 'minutesLabel') ??
+      _fallback.minutesLabel;
 
   @override
-  String get electricityCostSettingsLabel => LocalizelyGenL10n.getText(localeName, 'electricityCostSettingsLabel') ?? _fallback.electricityCostSettingsLabel;
+  String get spoolWeightLabel =>
+      LocalizelyGenL10n.getText(localeName, 'spoolWeightLabel') ??
+      _fallback.spoolWeightLabel;
 
   @override
-  String get submitButton => LocalizelyGenL10n.getText(localeName, 'submitButton') ?? _fallback.submitButton;
+  String get spoolCostLabel =>
+      LocalizelyGenL10n.getText(localeName, 'spoolCostLabel') ??
+      _fallback.spoolCostLabel;
 
   @override
-  String get resultElectricityPrefix => LocalizelyGenL10n.getText(localeName, 'resultElectricityPrefix') ?? _fallback.resultElectricityPrefix;
+  String get electricityCostLabel =>
+      LocalizelyGenL10n.getText(localeName, 'electricityCostLabel') ??
+      _fallback.electricityCostLabel;
 
   @override
-  String get resultFilamentPrefix => LocalizelyGenL10n.getText(localeName, 'resultFilamentPrefix') ?? _fallback.resultFilamentPrefix;
+  String get electricityCostSettingsLabel =>
+      LocalizelyGenL10n.getText(localeName, 'electricityCostSettingsLabel') ??
+      _fallback.electricityCostSettingsLabel;
 
   @override
-  String get resultTotalPrefix => LocalizelyGenL10n.getText(localeName, 'resultTotalPrefix') ?? _fallback.resultTotalPrefix;
+  String get submitButton =>
+      LocalizelyGenL10n.getText(localeName, 'submitButton') ??
+      _fallback.submitButton;
 
   @override
-  String get riskTotalPrefix => LocalizelyGenL10n.getText(localeName, 'riskTotalPrefix') ?? _fallback.riskTotalPrefix;
+  String get resultElectricityPrefix =>
+      LocalizelyGenL10n.getText(localeName, 'resultElectricityPrefix') ??
+      _fallback.resultElectricityPrefix;
 
   @override
-  String get premiumHeader => LocalizelyGenL10n.getText(localeName, 'premiumHeader') ?? _fallback.premiumHeader;
+  String get resultFilamentPrefix =>
+      LocalizelyGenL10n.getText(localeName, 'resultFilamentPrefix') ??
+      _fallback.resultFilamentPrefix;
 
   @override
-  String get labourCostPrefix => LocalizelyGenL10n.getText(localeName, 'labourCostPrefix') ?? _fallback.labourCostPrefix;
+  String get resultTotalPrefix =>
+      LocalizelyGenL10n.getText(localeName, 'resultTotalPrefix') ??
+      _fallback.resultTotalPrefix;
 
   @override
-  String get selectPrinterHint => LocalizelyGenL10n.getText(localeName, 'selectPrinterHint') ?? _fallback.selectPrinterHint;
+  String get riskTotalPrefix =>
+      LocalizelyGenL10n.getText(localeName, 'riskTotalPrefix') ??
+      _fallback.riskTotalPrefix;
 
   @override
-  String get watt => LocalizelyGenL10n.getText(localeName, 'watt') ?? _fallback.watt;
+  String get premiumHeader =>
+      LocalizelyGenL10n.getText(localeName, 'premiumHeader') ??
+      _fallback.premiumHeader;
 
   @override
-  String get kwh => LocalizelyGenL10n.getText(localeName, 'kwh') ?? _fallback.kwh;
+  String get labourCostPrefix =>
+      LocalizelyGenL10n.getText(localeName, 'labourCostPrefix') ??
+      _fallback.labourCostPrefix;
 
   @override
-  String get enterNumber => LocalizelyGenL10n.getText(localeName, 'enterNumber') ?? _fallback.enterNumber;
+  String get selectPrinterHint =>
+      LocalizelyGenL10n.getText(localeName, 'selectPrinterHint') ??
+      _fallback.selectPrinterHint;
 
   @override
-  String get invalidNumber => LocalizelyGenL10n.getText(localeName, 'invalidNumber') ?? _fallback.invalidNumber;
+  String get watt =>
+      LocalizelyGenL10n.getText(localeName, 'watt') ?? _fallback.watt;
 
   @override
-  String get supportEmailPrefix => LocalizelyGenL10n.getText(localeName, 'supportEmailPrefix') ?? _fallback.supportEmailPrefix;
+  String get kwh =>
+      LocalizelyGenL10n.getText(localeName, 'kwh') ?? _fallback.kwh;
 
   @override
-  String get supportEmail => LocalizelyGenL10n.getText(localeName, 'supportEmail') ?? _fallback.supportEmail;
+  String get enterNumber =>
+      LocalizelyGenL10n.getText(localeName, 'enterNumber') ??
+      _fallback.enterNumber;
 
   @override
-  String get supportIdLabel => LocalizelyGenL10n.getText(localeName, 'supportIdLabel') ?? _fallback.supportIdLabel;
+  String get invalidNumber =>
+      LocalizelyGenL10n.getText(localeName, 'invalidNumber') ??
+      _fallback.invalidNumber;
 
   @override
-  String get clickToCopy => LocalizelyGenL10n.getText(localeName, 'clickToCopy') ?? _fallback.clickToCopy;
+  String get supportEmailPrefix =>
+      LocalizelyGenL10n.getText(localeName, 'supportEmailPrefix') ??
+      _fallback.supportEmailPrefix;
 
   @override
-  String get materialWeightExplanation => LocalizelyGenL10n.getText(localeName, 'materialWeightExplanation') ?? _fallback.materialWeightExplanation;
+  String get supportEmail =>
+      LocalizelyGenL10n.getText(localeName, 'supportEmail') ??
+      _fallback.supportEmail;
 
   @override
-  String get exportSuccess => LocalizelyGenL10n.getText(localeName, 'exportSuccess') ?? _fallback.exportSuccess;
+  String get supportIdLabel =>
+      LocalizelyGenL10n.getText(localeName, 'supportIdLabel') ??
+      _fallback.supportIdLabel;
 
   @override
-  String get exportError => LocalizelyGenL10n.getText(localeName, 'exportError') ?? _fallback.exportError;
+  String get clickToCopy =>
+      LocalizelyGenL10n.getText(localeName, 'clickToCopy') ??
+      _fallback.clickToCopy;
 
   @override
-  String get exportButton => LocalizelyGenL10n.getText(localeName, 'exportButton') ?? _fallback.exportButton;
+  String get materialWeightExplanation =>
+      LocalizelyGenL10n.getText(localeName, 'materialWeightExplanation') ??
+      _fallback.materialWeightExplanation;
+
+  @override
+  String get exportSuccess =>
+      LocalizelyGenL10n.getText(localeName, 'exportSuccess') ??
+      _fallback.exportSuccess;
+
+  @override
+  String get exportError =>
+      LocalizelyGenL10n.getText(localeName, 'exportError') ??
+      _fallback.exportError;
+
+  @override
+  String get exportButton =>
+      LocalizelyGenL10n.getText(localeName, 'exportButton') ??
+      _fallback.exportButton;
 }
 
-class _LocalizelyLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+class _LocalizelyLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
   const _LocalizelyLocalizationsDelegate();
 
   @override
-  Future<AppLocalizations> load(Locale locale) => AppLocalizations.delegate.load(locale).then((appLocalizations) {
-    LocalizelyGenL10n.setCurrentLocale(appLocalizations.localeName);
-    return LocalizelyLocalizations(appLocalizations.localeName, appLocalizations);
-  });
+  Future<AppLocalizations> load(Locale locale) =>
+      AppLocalizations.delegate.load(locale).then((appLocalizations) {
+        LocalizelyGenL10n.setCurrentLocale(appLocalizations.localeName);
+        return LocalizelyLocalizations(
+          appLocalizations.localeName,
+          appLocalizations,
+        );
+      });
 
   @override
-  bool isSupported(Locale locale) => AppLocalizations.delegate.isSupported(locale);
+  bool isSupported(Locale locale) =>
+      AppLocalizations.delegate.isSupported(locale);
 
   @override
   bool shouldReload(_LocalizelyLocalizationsDelegate old) => false;
