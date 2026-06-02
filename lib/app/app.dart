bbe396966cef10b90fbd40298f3b1d5d8816757c
Reme Le Hane <mail@remej.dev>
2026-06-02 21:13:26 +0200

    chore: remove rate_my_app dependency
    Remove unused in-app rating package. Drop import and RateMyAppBuilder wrapper from App widget.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,6 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rate_my_app/rate_my_app.dart';
 import 'package:threed_print_cost_calculator/app/app_page.dart';
 import 'package:threed_print_cost_calculator/core/analytics/app_analytics.dart';
 import 'package:threed_print_cost_calculator/core/logging/app_logger.dart';
@@ -31,30 +30,7 @@ class App extends ConsumerWidget {
         theme: theme(),
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
-        home: RateMyAppBuilder(
-          rateMyApp: RateMyApp(
-            minDays: 3,
-            minLaunches: 7,
-            remindDays: 2,
-            remindLaunches: 5,
-            googlePlayIdentifier: 'com.threed_print_calculator',
-            appStoreIdentifier: 'com.threed-print-calculator',
-          ),
-          onInitialized: (context, rateMyApp) async {
-            if (rateMyApp.shouldOpenDialog) {
-              try {
-                rateMyApp.showRateDialog(context);
-              } catch (e) {
-                logger.warn(
-                  AppLogCategory.ui,
-                  'Rate dialog failed to open',
-                  error: e,
-                );
-              }
-            }
-          },
-          builder: (_) => const AppPage(),
-        ),
+        home: const AppPage(),
       ),
     );
   }
