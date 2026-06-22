ff1cabb1f6a0a322b11da4292d0761f2be2e82c8
Reme Le Hane <mail@remej.dev>
2026-06-22 09:41:14 +0200

    refactor(monitoring): Remove Firebase Crashlytics dependency

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'dart:io';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
