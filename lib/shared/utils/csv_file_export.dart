8fdb1b9dffe1c2ed0c40ea333ca2b4191bfd6c9a
Reme Le Hane <mail@remej.dev>
2026-07-22 17:27:04 +0200

    chore: Adds CSV file cleanup utilities

--- lib/shared/utils/csv_file_export.dart
+++ lib/shared/utils/csv_file_export.dart
@@ -5,6 +5,33 @@ import 'package:share_plus/share_plus.dart';
 import 'package:threed_print_cost_calculator/history/model/history_model.dart';
 import 'package:threed_print_cost_calculator/shared/utils/csv_generation.dart';
 
+const _appCsvRetention = Duration(days: 1);
+
+Future<void> cleanupStaleMaterialExportFiles() =>
+    _cleanupStaleCsvFiles('materials_');
+
+Future<void> cleanupStaleMaterialTemplateFiles() =>
+    _cleanupStaleCsvFiles('material_template_');
+
+Future<void> _cleanupStaleCsvFiles(String prefix) async {
+  try {
+    final directory = await getTemporaryDirectory();
+    final cutoff = DateTime.now().subtract(_appCsvRetention);
+    await for (final entity in directory.list(followLinks: false)) {
+      if (entity is! File ||
+          !entity.uri.pathSegments.last.startsWith(prefix) ||
+          !entity.path.endsWith('.csv')) {
+        continue;
+      }
+      try {
+        if ((await entity.stat()).modified.isBefore(cutoff)) {
+          await entity.delete();
+        }
+      } catch (_) {}
+    }
+  } catch (_) {}
+}
+
 Future<String> writeCsvToFile(String csv, {String? fileName}) async {
   final directory = await getTemporaryDirectory();
   final timestamp = DateTime.now().millisecondsSinceEpoch;
